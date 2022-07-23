26c35e64180f58614223a2dc895a8b3b866d24e0
Aman Menda <aman.menda@epitech.eu>
2022-07-23 18:25:59 -0400

    Log and default branch autosuggestion added

--- remoteAdd.sh
+++ remoteAdd.sh
@@ -18,7 +18,7 @@ git commit -m "$1"
 read -p $'Do you want to push on your current branch ? [Y/n]' VALIDATION
 
 if [ "$VALIDATION" == "y" ] || [ "$VALIDATION" == "Y" ] || [ -z "$VALIDATION" ]; then
-    git push origin `git status | head -n 1 | awk '{print $3}`
+    git push origin `git status | head -n 1 | awk '{print $3}'`
     echo $'remote-branch: origin\nlocal-branch: `git status | head -n 1 | awk '{print $3}`'
 else 
     read -p $'On which branch do you want to push ?\n' BRANCH
