a4b4fdd11b24e43a58c47d1fe81818be8fc1016c
Aman Menda <aman.menda@epitech.eu>
2022-07-23 16:25:00 -0400

    v0.1: initialisation

---
+++ remoteAdd.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+read -p $'Which files do you want to commit ?\n' FILES
+
+echo $'You are sending the following files:'
+echo $FILES
+#Display each file on a newline
+#Autocomplete
+#Number of element we are sending
+
+git add $FILES
+git commit -m "$1"
+
+read -p $'On which branch do you want to push ?\n' BRANCH
+echo 'remote-branch: origin\nlocal-branch:$BRANCH\n'
+git push origin $BRANCH
