b027d7d5093664efffe824a3652b79144b0ed302
Aman Menda <aman.menda@epitech.eu>
2022-07-23 17:57:02 -0400

    Log and default branch autosuggestion added

--- remoteAdd.sh
+++ remoteAdd.sh
@@ -5,16 +5,26 @@ echo $'Change log:'
 git status -s
 echo $''
 
+echo -n "On branch: "
+BR_NAME= git status | head -n 1 | awk '{print $3}'
+echo $''
+
 read -p $'Which files do you want to commit ?\n' FILES
-#Display each file on a newline
 #Autocomplete
 
 git add $FILES
 git commit -m "$1"
 
-read -p $'On which branch do you want to push ?\n' BRANCH
-echo $'remote-branch: origin\nlocal-branch: '$BRANCH''
-git push origin $BRANCH
+read -p $'Do you want to push on your current branch ('$BR_NAME') ? [Y/n]' VALIDATION
+
+if [ "$VALIDATION" == "y" ] || [ "$VALIDATION " == "Y"] || [ -z "$VALIDATION" ]; then
+    git push origin $BR_NAME
+    echo $'remote-branch: origin\nlocal-branch: '$BR_NAME''
+else 
+    read -p $'On which branch do you want to push ?\n' BRANCH
+    echo $'remote-branch: origin\nlocal-branch: '$BRANCH''
+    git push origin $BRANCH
+fi
 
 echo $'The actual state of your repository:'
 git ls-files
